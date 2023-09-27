f4007ea059ec697a27a32d17eb7b3b30585206df
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-09-27 13:53:06 -0300

    Feat: Componente Header

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return `<header>
+  <img src="./assets/img/logo.png" class="logo">
+  <nav>
+      <ul >
+          <li> <a href="index.html"> HOME </a> </li>
+          <li> <a href="./pages/produtos.html"> PRODUTOS </a> </li>
+          <li> <a href="./pages/sobre.html"> SOBRE </a> </li>
+          <li> <a href="./pages/contato.html"> CONTATO </a> </li>
+          <li class="login"> <a href="#"> LOGIN </a> </li>
+      </ul> 
+  </nav>
+</header>`;
+};
