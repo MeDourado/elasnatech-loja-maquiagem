edd4d78499d1f5bd41b0724b19bab5e0645dffdd
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-10-03 20:59:28 -0300

    Ajustes

---
+++ constants/produtos.js
@@ -0,0 +1,42 @@
+export const produtos = [
+  {
+    nome: "Kit de Pincel",
+    descricao: "Descrição do Produto 9",
+    preco: 19.99,
+    imagens: [
+      "../../assets/imgs/produto1.1.png",
+      "../../assets/imgs/produto1.2.png",
+      "../../assets/imgs/produto1.3.png",
+    ],
+  },
+  {
+    nome: "Lapis de Olho",
+    descricao: 30.99,
+    preco: 9.99,
+    imagens: [
+      "../../assets/imgs/produto2.1.png",
+      "../../assets/imgs/produto2.2.png",
+      "../../assets/imgs/produto2.3.png",
+    ],
+  },
+  {
+    nome: "Paleta de Cores",
+    descricao: 185.99,
+    preco: 58.99,
+    imagens: [
+      "../../assets/imgs/produto3.1.png",
+      "../../assets/imgs/produto3.2.png",
+      "../../assets/imgs/produto3.3.png",
+    ],
+  },
+  {
+    nome: "Brilho Labial",
+    descricao: 85.99,
+    preco: 36.99,
+    imagens: [
+      "../../assets/imgs/produto4.1.png",
+      "../../assets/imgs/produto4.2.png",
+      "../../assets/imgs/produto4.3.png",
+    ],
+  },
+];
